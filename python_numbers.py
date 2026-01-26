ce15ff0b7c03d85bc123eef3ee15675ac145188a
hemapriya <hema.senthilkumar@outlook.com>
2026-01-26 17:41:38 +0530

    adding numbers

---
+++ python_numbers.py
@@ -0,0 +1,25 @@
+"""
+Numeric Data Types
+- Integers
+- Floats
+- Complex
+
+Integers
+ - whole numbers positive or negative
+ - unbounded (they can be large as they want)
+ - they cannot start with leading zeros
+Floats 
+- Fractional numbers postive or negative
+- bounded
+- they have limits of precision
+- they take up more space 
+- they can/or not have leading zeros
+
+Numerical Notations
+we can use 1_000 for 1,000 (underscores instead of commas) # its purely for human sake and not for python sake
+for float as well 1,000.55 => 1_000.55
+
+we can use scientific notation also
+1.286e+19 => add that many zeros
+6.7e-3 => 0.0067
+"""
